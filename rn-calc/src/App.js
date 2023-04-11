b349466d422d0a859409829c78beb88292a0661b
혜민 <97522726+heymin2@users.noreply.github.com>
2023-04-12 00:30:02 +0900

    2.2 컴포넌트와 스타일

--- rn-calc/src/App.js
+++ rn-calc/src/App.js
@@ -2,9 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const isError = false;
   return (
     <View style={styles.container}>
-      <Text>rn-calc</Text>
+      <Text
+        style={{
+          fontSize: 30,
+          fontWeight: '700',
+          color: 'blue',
+          backgroundColor: 'black',
+          padding: 10,
+          borderWidth: 3,
+          borderColor: 'red',
+          // 인라인 스타일
+        }}
+      >
+        rn-calc App
+      </Text>
+      <Text style={styles.text}>rn-calc App2</Text>
+      <Text style={[styles.text, styles.error]}>error</Text>
+      <Text style={[styles.text, isError && styles.error]}>error</Text>
+      {/* 앞에 스타일 먼저 적용, 뒤가 덮어씀, true일때 덮어씀 */}
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +34,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: '700',
+    color: 'blue',
+    backgroundColor: 'green',
+    padding: 10,
+    borderWidth: 3,
+    borderColor: 'red',
+  },
+  error: {
+    color: 'red',
   },
 });
