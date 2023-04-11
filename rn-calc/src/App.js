0abc4510d5daddb6c00921b1557a90ac4f3acf33
혜민 <97522726+heymin2@users.noreply.github.com>
2023-04-12 00:48:12 +0900

    2.3 Button 컴포넌트와 props

--- rn-calc/src/App.js
+++ rn-calc/src/App.js
@@ -1,28 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const isError = false;
   return (
     <View style={styles.container}>
-      <Text
-        style={{
-          fontSize: 30,
-          fontWeight: '700',
-          color: 'blue',
-          backgroundColor: 'black',
-          padding: 10,
-          borderWidth: 3,
-          borderColor: 'red',
-          // 인라인 스타일
-        }}
-      >
-        rn-calc App
-      </Text>
-      <Text style={styles.text}>rn-calc App2</Text>
-      <Text style={[styles.text, styles.error]}>error</Text>
-      <Text style={[styles.text, isError && styles.error]}>error</Text>
-      {/* 앞에 스타일 먼저 적용, 뒤가 덮어씀, true일때 덮어씀 */}
+      <Text style={styles.text}>rn-calc App</Text>
+      <Button
+        title="button"
+        color={'red'}
+        onPress={() => console.log('click!')}
+      />
+      {/* 안드로이드는 타이틀이 대문자로 변경됨, 배경 생김, 
+      title 무조건 지정해줘야함 => 버튼 구현 부분에서 검사해서 오류 뜸, 
+      onPress는 require인데 지정 안해도 오류x => 버튼 구현 부분에서 검사x, 
+      style props 지원 안함, 배경 변경할때 color */}
       <StatusBar style="auto" />
     </View>
   );
@@ -39,12 +30,5 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: '700',
     color: 'blue',
-    backgroundColor: 'green',
-    padding: 10,
-    borderWidth: 3,
-    borderColor: 'red',
-  },
-  error: {
-    color: 'red',
   },
 });
