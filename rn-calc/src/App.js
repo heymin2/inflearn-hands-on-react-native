25ce46853e24d0751e3e0e3a8566d9d18220b654
혜민 <97522726+heymin2@users.noreply.github.com>
2023-04-16 15:53:52 +0900

    3.5 버튼 크기와 구분선

--- rn-calc/src/App.js
+++ rn-calc/src/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, useWindowDimensions } from 'react-native';
 import Button, { ButtonTypes } from './components/Button';
 import { useState } from 'react';
 
 export default function App() {
   const [result, setResult] = useState(0);
+  const width = (useWindowDimensions().width - 5) / 4;
+  console.log(width);
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
@@ -14,6 +16,26 @@ export default function App() {
       <View style={styles.buttonContainer}>
         <View style={styles.leftPad}>
           <View style={styles.number}></View>
+          <Button
+            title="1"
+            onPress={() => {}}
+            buttonStyle={{ width, height: width, marginBottom: 1 }}
+          />
+          <Button
+            title="2"
+            onPress={() => {}}
+            buttonStyle={{ width, height: width, marginBottom: 1 }}
+          />
+          <Button
+            title="3"
+            onPress={() => {}}
+            buttonStyle={{ width, height: width, marginBottom: 1 }}
+          />
+          <Button
+            title="4"
+            onPress={() => {}}
+            buttonStyle={{ width, height: width, marginBottom: 1 }}
+          />
           <View style={styles.bottom}></View>
         </View>
         <View style={styles.operator}></View>
@@ -37,7 +59,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#000000',
   },
   buttonContainer: {
-    flex: 1,
     flexDirection: 'row',
     backgroundColor: 'skyblue',
   },
