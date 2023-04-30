6a1cdf8e096b195f26ebafba05c80d51296af911
혜민 <97522726+heymin2@users.noreply.github.com>
2023-04-30 23:02:15 +0900

    4.6 TextInput 컴포넌트의 다양한 props

--- rn-todo/src/components/Input.js
+++ rn-todo/src/components/Input.js
@@ -1,23 +1,55 @@
 import { StyleSheet, View, Text, TextInput } from 'react-native';
 import PropTypes from 'prop-types';
 
-const Input = ({ title, placeholder }) => {
+export const KeyboardTypes = {
+  DEFAULT: 'default',
+  EMAIL: 'email-address',
+};
+
+export const ReturnKeyTypes = {
+  DONE: 'done',
+  NEXT: 'next',
+};
+
+const Input = ({
+  title,
+  placeholder,
+  //   keyboardType,
+  //   returnKeyType,
+  //   secureTextEntry,
+  ...props
+}) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
 
       <TextInput
+        {...props} // 제일 위에 둬야함. 덮어써지지 않아야함
         style={styles.input}
         placeholder={placeholder ?? title}
         placeholderTextColor={'#a3a3a3'}
+        autoCapitalize={'none'}
+        autoCorrect={false}
+        textContentType={'none'}
+        keyboardAppearance={'light'}
+        // keyboardType={keyboardType}
+        // returnKeyType={returnKeyType}
+        // secureTextEntry={secureTextEntry}
       />
     </View>
   );
 };
 
+Input.defaultProps = {
+  returnKeyType: ReturnKeyTypes.DONE,
+};
+
 Input.PropTypes = {
   title: PropTypes.string,
   placeholder: PropTypes.string,
+  //   keyboardType: PropTypes.oneOf(Object.values(KeyboardTypes)),
+  //   returnKeyType: PropTypes.oneOf(Object.values(ReturnKeyTypes)),
+  //   secureTextEntry: PropTypes.bool,
 };
 
 const styles = StyleSheet.create({
--- rn-todo/src/screens/SignInScreen.js
+++ rn-todo/src/screens/SignInScreen.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Image, View } from 'react-native';
-import Input from '../components/Input';
+import Input, { KeyboardTypes, ReturnKeyTypes } from '../components/Input';
 
 const SignInScreen = () => {
   return (
@@ -9,8 +9,13 @@ const SignInScreen = () => {
         style={styles.image}
         resizeMode={'cover'}
       />
-      <Input title={'email'} placeholder={'your@email.com'} />
-      <Input title={'password'} />
+      <Input
+        title={'email'}
+        placeholder={'your@email.com'}
+        keyboardType={KeyboardTypes.EMAIL}
+        returnKeyType={ReturnKeyTypes.NEXT}
+      />
+      <Input title={'password'} secureTextEntry />
     </View>
   );
 };
