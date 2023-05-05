e705da97bab83e67de13389b3b5b51356263d24a
혜민 <97522726+heymin2@users.noreply.github.com>
2023-05-05 17:02:50 +0900

    5.5 헤더 타이틀

--- rn-todo/src/navigations/AuthStack.js
+++ rn-todo/src/navigations/AuthStack.js
@@ -1,19 +1,44 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignInScreen from '../screens/SignInScreen';
 import ListScreen from '../screens/ListScreen';
-import { WHITE } from '../color';
+import { PRIMARY, WHITE } from '../color';
+import { Pressable, Text } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 const AuthStack = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        contentStyle: { backgroundColor: WHITE },
+        headerTitleAlign: 'center',
+        headerTintColor: PRIMARY.DEFAULT,
+        headerTitleStyle: { fontWeight: '700' },
+      }}
+    >
       <Stack.Screen
-        name={'SignIn'}
-        component={SignInScreen}
-        options={{ contentStyle: { backgroundColor: WHITE } }}
+        name={'List'}
+        component={ListScreen}
+        // options={{
+        //   title: 'TODO LIST',
+        // }}
       />
-      <Stack.Screen name={'List'} component={ListScreen} />
+      <Stack.Screen
+        name={'Home'}
+        component={SignInScreen}
+        // options={{
+        //   title: 'TODO LIST',
+        // headerTitle: (props) => {
+        //   console.log(props);
+        //   return (
+        //     <Pressable onPress={() => console.log('header')}>
+        //       <Text style={{ color: props.tintColor }}>{props.children}</Text>
+        //     </Pressable>
+        //   );
+        // },
+        // }}
+      />
     </Stack.Navigator>
   );
 };
