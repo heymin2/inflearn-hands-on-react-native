f5ca3189839a566e075e23e3c7836665a8177c03
혜민 <97522726+heymin2@users.noreply.github.com>
2023-05-14 01:34:16 +0900

    6.3 FlatList 컴포넌트 디자인하기

--- rn-todo/src/screens/ListScreen.js
+++ rn-todo/src/screens/ListScreen.js
@@ -1,32 +1,40 @@
-import { memo } from 'react';
-import { FlatList, StyleSheet, View, Text } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
+import ListItem from '../components/ListItem';
+import { GRAY } from '../color';
 
-const ListItem = memo(({ item }) => {
-  console.log(item.id);
-  return (
-    <View style={{ paddingHorizontal: 20, paddingVertical: 10 }}>
-      <Text style={{ fontSize: 20 }}>{item.task}</Text>
-    </View>
-  );
+const Separator = () => {
+  return <View style={styles.separator}></View>;
+};
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 1,
+    backgroundColor: GRAY.LIGHT,
+    marginVertical: 10,
+    marginHorizontal: 10,
+  },
 });
 
 const ListScreen = () => {
-  const todos = [];
-  for (let i = 1; i < 501; i++) {
-    todos.push({ id: i, task: `task ${i}` });
-  }
+  const todos = [
+    { id: 1, task: 'React Native 1', isDone: false },
+    { id: 2, task: 'React Native 2', isDone: true },
+    { id: 3, task: 'React Native 3', isDone: true },
+    { id: 4, task: 'React Native 4', isDone: false },
+    { id: 5, task: 'React Native 5', isDone: false },
+  ];
 
   return (
     <FlatList
       data={todos}
       renderItem={({ item }) => <ListItem item={item} />}
-      keyExtractor={(item) => item.id.toString()}
       windowSize={5} // 이전 :2, 현재:1, 다음: 2
+      ItemSeparatorComponent={Separator}
+      // ListHeaderComponent={() => <View style={{ height: 10 }}></View>}
+      ListHeaderComponent={View}
+      ListHeaderComponentStyle={{ height: 10 }} // 위랑 동일함
     />
   );
 };
 
-const styles = StyleSheet.create({
-  container: { flex: 1 },
-});
 export default ListScreen;
