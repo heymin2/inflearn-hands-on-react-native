19c04c387e50f787eed722afd620e0a3dfcf0f23
혜민 <97522726+heymin2@users.noreply.github.com>
2023-05-19 23:21:37 +0900

    7.11 배경 이미지 사용하기

--- rn-photo/src/screens/SignInScreen.js
+++ rn-photo/src/screens/SignInScreen.js
@@ -1,4 +1,4 @@
-import { Keyboard, StyleSheet, View } from 'react-native';
+import { Image, Keyboard, StatusBar, StyleSheet, View } from 'react-native';
 import Input, { InputTypes } from '../components/Input';
 import { useEffect, useRef, useState } from 'react';
 import Button from '../components/Button';
@@ -8,6 +8,7 @@ import TextButton from '../components/TextButton';
 import { useNavigation } from '@react-navigation/native';
 import { AuthRoutes } from '../navigations/routes';
 import HR from '../components/HR';
+import { WHITE } from '../colors';
 
 const SignInScreen = () => {
   const [email, setEmail] = useState('');
@@ -16,7 +17,7 @@ const SignInScreen = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [disabled, setDisabled] = useState(true);
 
-  const { top } = useSafeAreaInsets();
+  const { top, bottom } = useSafeAreaInsets();
   const { navigate } = useNavigation(); // navigation으로도 가능한데 navigate만 필요하니까 {navigate}만 받아옴
 
   useEffect(() => {
@@ -34,36 +35,48 @@ const SignInScreen = () => {
 
   return (
     <SafeInputView>
+      <StatusBar style={'light'} />
       <View style={[styles.container, { paddingTop: top }]}>
-        <Input
-          inputType={InputTypes.EMAIL}
-          value={email}
-          onChangeText={(text) => setEmail(text.trim())}
-          onSubmitEditing={() => passwordRef.current.focus()}
-          styles={{ container: { marginBottom: 20 } }}
-        />
-        <Input
-          ref={passwordRef}
-          inputType={InputTypes.PASSWORD}
-          value={password}
-          onChangeText={(text) => setPassword(text.trim())}
-          onSubmitEditing={onSubmit}
-          styles={{ container: { marginBottom: 20 } }}
-        />
-        <Button
-          title={'SIGNIN'}
-          disabled={disabled}
-          isLoading={isLoading}
-          onPress={onSubmit}
-          styles={{ container: { marginTop: 20 } }}
-        />
-        <HR text={'OR'} />
-        <TextButton
-          title={'SIGNUP'}
-          onPress={() => {
-            navigate(AuthRoutes.SIGN_UP);
-          }}
-        />
+        <View style={StyleSheet.absoluteFillObject}>
+          <Image
+            source={require('../../assets/cover.png')}
+            style={{ width: '100%' }}
+            resizeMode={'cover'}
+          />
+        </View>
+        <View
+          style={[styles.form, { paddingBottom: bottom ? bottom + 10 : 40 }]}
+        >
+          <Input
+            inputType={InputTypes.EMAIL}
+            value={email}
+            onChangeText={(text) => setEmail(text.trim())}
+            onSubmitEditing={() => passwordRef.current.focus()}
+            styles={{ container: { marginBottom: 20 } }}
+          />
+          <Input
+            ref={passwordRef}
+            inputType={InputTypes.PASSWORD}
+            value={password}
+            onChangeText={(text) => setPassword(text.trim())}
+            onSubmitEditing={onSubmit}
+            styles={{ container: { marginBottom: 20 } }}
+          />
+          <Button
+            title={'SIGNIN'}
+            disabled={disabled}
+            isLoading={isLoading}
+            onPress={onSubmit}
+            styles={{ container: { marginTop: 20 } }}
+          />
+          <HR text={'OR'} styles={{ container: { marginVertical: 30 } }} />
+          <TextButton
+            title={'SIGNUP'}
+            onPress={() => {
+              navigate(AuthRoutes.SIGN_UP);
+            }}
+          />
+        </View>
       </View>
     </SafeInputView>
   );
@@ -72,12 +85,18 @@ const SignInScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 20,
+    justifyContent: 'flex-end',
   },
   title: {
     fontSize: 30,
+  },
+  form: {
+    alignItems: 'center',
+    backgroundColor: WHITE,
+    paddingHorizontal: 20,
+    paddingTop: 40,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
   },
 });
 
