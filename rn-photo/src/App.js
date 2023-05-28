d40d64835c6fd1461b2490bc82ce8d32dc87ac67
혜민 <97522726+heymin2@users.noreply.github.com>
2023-05-28 22:22:45 +0900

    7.20 Assets 캐싱하기

--- rn-photo/src/App.js
+++ rn-photo/src/App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import Navigation from './navigations';
+import * as SpalshScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import { Asset } from 'expo-asset';
 
 const App = () => {
+  const [isReady, setIsReady] = useState(false);
+  useEffect(() => {
+    (async () => {
+      try {
+        await SpalshScreen.preventAutoHideAsync();
+        await Asset.fromModule(require('../assets/cover.png')).downloadAsync(); // 캐싱
+      } catch (e) {
+        // eslint-disabled-next-line no-console
+        console.log(e);
+      } finally {
+        setIsReady(true);
+      }
+    })();
+  }, []);
+
+  const onReady = async () => {
+    if (isReady) {
+      await SpalshScreen.hideAsync();
+    }
+  };
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
-    <>
+    <View style={{ flex: 1 }} onLayout={onReady}>
       <StatusBar style={'dark'} />
       <Navigation />
-    </>
+    </View>
   );
 };
 
