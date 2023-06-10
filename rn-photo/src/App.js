fc0d9962728639247cba4993d1ed27b77838c1b1
혜민 <97522726+heymin2@users.noreply.github.com>
2023-06-11 00:10:22 +0900

    8.8 로그인 유지하기

--- rn-photo/src/App.js
+++ rn-photo/src/App.js
@@ -1,48 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import Navigation from './navigations';
-import * as SpalshScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
-import { LogBox, View } from 'react-native';
-import { Asset } from 'expo-asset';
-import { initFirebase } from './api/firebase';
+import { LogBox } from 'react-native';
 import { UserProvier } from './contexts/UserContext';
 
 const App = () => {
   LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core']);
 
-  const [isReady, setIsReady] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        await SpalshScreen.preventAutoHideAsync();
-        await Asset.fromModule(require('../assets/cover.png')).downloadAsync(); // 캐싱
-        initFirebase();
-      } catch (e) {
-        // eslint-disabled-next-line no-console
-        console.log(e);
-      } finally {
-        setIsReady(true);
-      }
-    })();
-  }, []);
-
-  const onReady = async () => {
-    if (isReady) {
-      await SpalshScreen.hideAsync();
-    }
-  };
-
-  if (!isReady) {
-    return null;
-  }
-
   return (
     <UserProvier>
-      <View style={{ flex: 1 }} onLayout={onReady}>
-        <StatusBar style={'dark'} />
-        <Navigation />
-      </View>
+      <StatusBar style={'dark'} />
+      <Navigation />
     </UserProvier>
   );
 };
